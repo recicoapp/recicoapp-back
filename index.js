b61d6df1fa9c89fe835a03faa41f815c7e378454
David R <deveeup@MacBook-Pro.local>
2020-05-30 09:58:32 -0500

    add .env

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var express = require("express");
 var app = express();
+const { config } = require("./config");
 
 app.get("/", function (req, res) {
-  res.send("GlobHack 2020 - Be4Tech");
+  res.send("GlobHack 2020 - home");
 });
 
 app.get("/test", function (req, res) {
-  res.send("GlobHack 2020 - Be4Tech");
+  res.send("GlobHack 2020 - get");
 });
 app.post("/post", function (req, res) {
-  res.send("GlobHack 2020 - Be4Tech");
+  res.send("GlobHack 2020 - post");
 });
 
-app.listen(3001, function () {
-  console.log(`Run :)`);
+app.listen(config.port, function () {
+  console.log(`happy code :D`);
 });
