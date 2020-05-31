5c0afdf227fdbd40e497c97e74ce677319a088a0
leonardo <clgrhc@gmail.com>
2020-05-31 09:10:23 -0500

    added code for avoid cors errors

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// enable cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  next();
+});
+
+
 //global config of routes
 app.use(require("./routes/index"));
 
